cf750329e130b7691a8c8d7dc6f9b4b4459906d6
David North <bigchimp2005+github@gmail.com>
2015-07-17 11:56:50 +0100

    Changes the default .Net version to 2.0
    We may end up upgrading it, but given that Unity uses a .Net 2.0 subset
    this is probably the best approach

--- PurpleKingdomGamesCore/Properties/AssemblyInfo.cs
+++ PurpleKingdomGamesCore/Properties/AssemblyInfo.cs
@@ -1,20 +1,27 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PurpleKingdomGamesCore")]
+[assembly: AssemblyTitle("Purple Kingdom Games Core Libs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PurpleKingdomGamesCore")]
+[assembly: AssemblyCompany("Purple Kingdom Games Ltd.")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c84dd7c-78c1-4fa6-907f-8cee7d9fc4f8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-GB")]
