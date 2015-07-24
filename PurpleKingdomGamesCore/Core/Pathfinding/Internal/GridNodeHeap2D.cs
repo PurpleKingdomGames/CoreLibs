2fd797cd7b9c8733240973a0263d0f2c2e0cb02e
David North <bigchimp2005+github@gmail.com>
2015-07-24 12:57:20 +0100

    Adds specialised heal for grid nodes

---
+++ PurpleKingdomGamesCore/Core/Pathfinding/Internal/GridNodeHeap2D.cs
@@ -0,0 +1,45 @@
+﻿using PurpleKingdomGames.Core.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace PurpleKingdomGames.Core.Pathfinding.Internal
+{
+    internal class GridNodeHeap2D : BinaryHeap<GridNodeCalc2D>
+    {
+        private Dictionary<int, Dictionary<int, GridNodeCalc2D>> graphHeap = new Dictionary<int, Dictionary<int, GridNodeCalc2D>>();
+
+        public new void Add(GridNodeCalc2D item)
+        {
+            Point2D pos = item.ReferenceNode.Position;
+            if (!graphHeap.ContainsKey((int) pos.X)) {
+                graphHeap.Add((int) pos.X, new Dictionary<int, GridNodeCalc2D>());
+            }
+
+            graphHeap[(int) pos.X][(int) pos.Y] = item;
+
+            base.Add(item);
+        }
+
+        public new GridNodeCalc2D Remove()
+        {
+            GridNodeCalc2D item = base.Remove();
+            Point2D pos = item.ReferenceNode.Position;
+            graphHeap[(int) pos.X].Remove((int) pos.Y);
+
+            return item;
+        }
+
+        public GridNodeCalc2D GetByPosition(Point2D position)
+        {
+            if (position.X < 0 || position.Y < 0) {
+                throw new Exception("X and Y cannot be less than zero");
+            }
+
+            if (!graphHeap.ContainsKey((int) position.X) || !graphHeap[(int) position.X].ContainsKey((int) position.Y)) {
+                return null;
+            }
+
+            return graphHeap[(int) position.X][(int) position.Y];
+        }
+    }
+}
