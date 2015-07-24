92e5d26aa03cae2e1edf8276a8fee46e5137659b
David North <bigchimp2005+github@gmail.com>
2015-07-24 12:12:03 +0100

    Adds comments to grid nodes and 2D points

--- PurpleKingdomGamesCore/Core/Pathfinding/GridNode2D.cs
+++ PurpleKingdomGamesCore/Core/Pathfinding/GridNode2D.cs
@@ -2,14 +2,42 @@
 
 namespace PurpleKingdomGames.Core.Pathfinding
 {
+    /// <summary>
+    /// Stores a node on a grid with a location in world space, penalty, and whether it's passable
+    /// </summary>
     public struct GridNode2D
     {
-        public Point2D Position { get; set; }
-        public Point2D GridPosition { get; set; }
+        /// <summary>
+        /// Get the position of this node in world space
+        /// </summary>
+        public readonly Point2D Position;
+
+        /// <summary>
+        /// Whether or not a path can cross through this node
+        /// </summary>
         public bool Passable { get; set; }
+
+        /// <summary>
+        /// The penalty for passing through this node
+        /// </summary>
         public int Penalty { get; set; }
 
-        public GridNodeCalc2D ToGridNodeCalc2D()
+        /// <summary>
+        /// Create a new grid node
+        /// </summary>
+        /// <param name="position">The position in world space</param>
+        public GridNode2D(Point2D position)
+        {
+            Position = position;
+            Passable = false;
+            Penalty = 0;
+        }
+
+        /// <summary>
+        /// Convert a grid node to an internal calculated grid node (used for pathfinding)
+        /// </summary>
+        /// <returns></returns>
+        internal GridNodeCalc2D ToGridNodeCalc2D()
         {
             return new GridNodeCalc2D() { ReferenceNode = this };
         }
--- PurpleKingdomGamesCore/Core/Point2D.cs
+++ PurpleKingdomGamesCore/Core/Point2D.cs
@@ -1,22 +1,48 @@
 ﻿
 namespace PurpleKingdomGames.Core
 {
+    /// <summary>
+    /// A single 2-dimensional point in world space
+    /// </summary>
     public struct Point2D
     {
+        /// <summary>
+        /// The X position of this point
+        /// </summary>
         public float X;
+
+        /// <summary>
+        /// The Y position of this point
+        /// </summary>
         public float Y;
 
+        /// <summary>
+        /// Creates a new point in world space with the co-ordinates supplied
+        /// </summary>
+        /// <param name="x">The x position of this point</param>
+        /// <param name="y">The y position of this point</param>
         public Point2D(float x, float y)
         {
             X = x;
             Y = y;
         }
 
+        /// <summary>
+        /// Adds 2 points together by adding the X and Y positions
+        /// </summary>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        /// <returns></returns>
         public static Point2D operator +(Point2D point1, Point2D point2)
         {
             return new Point2D() { X = point1.X + point2.X, Y = point1.Y + point2.Y };
         }
 
+        /// <summary>
+        /// Determines if 2 objects are  equal
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public override bool Equals(object obj)
         {
             if (obj is Point2D) {
@@ -26,26 +52,46 @@ namespace PurpleKingdomGames.Core
             }
         }
 
+        /// <summary>
+        /// Determines if 2 points are equal by comparing their X and Y values
+        /// </summary>
+        /// <param name="point">The point to compare</param>
+        /// <returns></returns>
         public bool Equals(Point2D point)
         {
             return this == point;
         }
 
+        /// <summary>
+        /// etermines whether the two points are different from each other
+        /// </summary>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        /// <returns></returns>
         public static bool operator !=(Point2D point1, Point2D point2)
         {
             return (point1.X != point2.X || point1.Y != point2.Y);
         }
 
+        /// <summary>
+        /// Determines if 2 points are equal by comparing their X and Y values
+        /// </summary>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        /// <returns></returns>
         public static bool operator ==(Point2D point1, Point2D point2)
         {
             return (point1.X == point2.X && point1.Y == point2.Y);
         }
-
-        // See: http://answers.unity3d.com/questions/908269/c-overloading-equals-what-is-it-about.html#answer-908295
+        
+        /// <summary>
+        /// Identify an object in a hash-based collection
+        /// </summary>
+        /// <returns></returns>
         public override int GetHashCode()
         {
+            // http://answers.unity3d.com/questions/908269/c-overloading-equals-what-is-it-about.html#answer-908295
             int hash = 17;
-            // Suitable nullity checks etc, of course :)
             hash = hash * 23 + X.GetHashCode();
             hash = hash * 23 + Y.GetHashCode();
             return hash;
