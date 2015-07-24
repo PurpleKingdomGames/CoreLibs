e4251e88dc088357f1c821545d4b52eb6563b39e
David North <bigchimp2005+github@gmail.com>
2015-07-24 12:47:33 +0100

    Adds initial Astar class for seeking targets

---
+++ PurpleKingdomGamesCore/Core/Pathfinding/Seekers/AStar.cs
@@ -0,0 +1,22 @@
+﻿
+namespace PurpleKingdomGames.Core.Pathfinding.Seekers
+{
+    /// <summary>
+    /// Contains methods for seeking a target within a specified grid using the A* pathfind algorith
+    /// </summary>
+    public static class AStar
+    {
+        /// <summary>
+        /// Seek a target in a 2-dimensional grid
+        /// </summary>
+        /// <param name="grid">The grid to search</param>
+        /// <param name="start">The start point to seek from</param>
+        /// <param name="target">The target to seek to</param>
+        /// <returns>An array of grid nodes needed to pass through to get to the target</returns>
+        public static GridNode2D[] Seek(GridNode2D[,] grid, Point2D start, Point2D target)
+        {
+
+            return new GridNode2D[0];
+        }
+    }
+}
