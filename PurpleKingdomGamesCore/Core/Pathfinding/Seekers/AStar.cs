bbc60c4ca69ee29d7fc6ee055d1e877f62346e2f
David North <bigchimp2005+github@gmail.com>
2015-07-31 10:23:40 +0100

    Moves complex calculations into a separate method

--- PurpleKingdomGamesCore/Core/Pathfinding/Seekers/AStar.cs
+++ PurpleKingdomGamesCore/Core/Pathfinding/Seekers/AStar.cs
@@ -47,10 +47,6 @@ namespace PurpleKingdomGames.Core.Pathfinding.Seekers
         /// </exception>
         public static Point2D[] Seek(GridNode2D[,] grid, Point2D start, Point2D target, LockType lockType, int maxDescent, int maxClimb, bool cutCorners)
         {
-            // Calculate possible paths
-            GridNodeHeap2D openNodes = new GridNodeHeap2D();
-            List<GridNode2D> closedNodes = new List<GridNode2D>();
-
             // Check that the starting position is not out of range of the grid
             if (start.X > grid.GetLength(0) || start.X < 0 || start.Y >= grid.GetLength(1) || start.Y < 0) {
                 throw new ArgumentOutOfRangeException(
@@ -80,6 +76,8 @@ namespace PurpleKingdomGames.Core.Pathfinding.Seekers
                 }
             }
 
+            GridNodeHeap2D openNodes = new GridNodeHeap2D();
+            List<GridNode2D> closedNodes = new List<GridNode2D>();
             GridNodeCalc2D currentNode = grid[(int) start.X, (int) start.Y].ToGridNodeCalc2D();
             GridNodeCalc2D targetNode = null;
 
@@ -112,95 +110,8 @@ namespace PurpleKingdomGames.Core.Pathfinding.Seekers
                 // Add the current node to the closed list
                 closedNodes.Add(currentNode.ReferenceNode);
 
-                Point2D currentGridPos = currentNode.GridPosition;
-
-                // Make sure the X and Y coordinates are always within the grid boundaries
-                int minX = (int) Math.Max(currentGridPos.X - 1, 0);
-                int maxX = (int) Math.Min(currentGridPos.X + 1, grid.GetLength(0));
-                int minY = (int) Math.Max(currentGridPos.Y - 1, 0);
-                int maxY = (int) Math.Min(currentGridPos.Y + 1, grid.GetLength(1));
-
-                for (int x = minX; x <= maxX; x++) {
-                    for (int y = minY; y <= maxY; y++) {
-                        GridNodeCalc2D node = grid[x, y].ToGridNodeCalc2D();
-                        node.GridPosition = new Point2D(x, y);
-
-                        // Skip over this node if it's the one we're currently looking at, or it's in the closed list
-                        if (node.GridPosition == currentGridPos || closedNodes.Contains(node.ReferenceNode)) {
-                            continue;
-                        }
-
-                        // Also skip if it's impassable
-                        if (node.ReferenceNode.Passable == false) {
-                            continue;
-                        }
-
-                        GridNodeCalc2D refNode = openNodes.GetByPosition(node.GridPosition);
-                        bool isDiagonal = (
-                            node.GridPosition.X != currentGridPos.X &&
-                            node.GridPosition.Y != currentGridPos.Y
-                        );
-
-                        // If we're not meant to cut corners, tehn we need to check this now
-                        if (isDiagonal && !cutCorners) {
-                            // If we are further down the grid, then check the square
-                            // above to make sure it's also passable
-                            if (refNode.GridPosition.Y > 0 && refNode.GridPosition.Y > currentGridPos.Y) {
-                                if (!grid[(int) refNode.GridPosition.X, (int) (refNode.GridPosition.Y - 1)].Passable) {
-                                    continue;
-                                }
-                            }
-
-                            // If we are further up the grid, then check the square
-                            // below to make sure it's also passable
-                            if (refNode.GridPosition.Y < maxY && refNode.GridPosition.Y < currentGridPos.Y) {
-                                if (!grid[(int) refNode.GridPosition.X, (int) (refNode.GridPosition.Y + 1)].Passable) {
-                                    continue;
-                                }
-                            }
-                        }
-
-                        // Is this node on the open list already?
-                        if (refNode == null) {
-                            // If not then set the parent to the current node and calculate the cost
-                            node.Parent = currentNode;
-                            node.MovementCost = currentNode.TotalCost + 10;
-
-                            // Diagonal... add a bit
-                            if (isDiagonal) {
-                                node.MovementCost += 4;
-                            }
-
-                            // Calculate the heuristic cost
-                            node.HeuristicCost = (int) (
-                                Math.Abs(node.GridPosition.X - target.X) +
-                                Math.Abs(node.GridPosition.Y - target.Y)
-                            );
-
-                            openNodes.Add(node);
-                            continue;
-                        }
-
-                        int currentNodeCost = currentNode.MovementCost + currentNode.ReferenceNode.Penalty;
-                        int refNodeCost = refNode.MovementCost + refNode.ReferenceNode.Penalty;
-
-                        // The node was in the open list, so see if it costs less to move there
-                        if (refNodeCost < currentNodeCost) {
-                            // If so, set then set the parent to the current node
-                            refNode.Parent = currentNode.Parent;
-
-                            // Re-calculate the movement cost using the new parent
-                            refNode.MovementCost = refNode.Parent.TotalCost + 10;
-
-                            // Diagonal... add a bit
-                            if (isDiagonal) {
-                                refNode.MovementCost += 4;
-                            }
-
-                            openNodes.Sort(openNodes.IndexOf(refNode));
-                        }
-                    }
-                }
+                // Add additional nodes to the open list, ready to search
+                calculateOpenList(currentNode, target, grid, openNodes, closedNodes, cutCorners);
             }
 
             if (targetNode == null) {
@@ -221,6 +132,99 @@ namespace PurpleKingdomGames.Core.Pathfinding.Seekers
 
             // Return the new array of path points
             return path.ToArray();
+        }
+
+        private static void calculateOpenList(GridNodeCalc2D currentNode, Point2D target, GridNode2D[,] grid, GridNodeHeap2D openNodes, List<GridNode2D> closedNodes, bool cutCorners)
+        {
+            Point2D currentGridPos = currentNode.GridPosition;
+
+            // Make sure the X and Y coordinates are always within the grid boundaries
+            int minX = (int) Math.Max(currentGridPos.X - 1, 0);
+            int maxX = (int) Math.Min(currentGridPos.X + 1, grid.GetLength(0));
+            int minY = (int) Math.Max(currentGridPos.Y - 1, 0);
+            int maxY = (int) Math.Min(currentGridPos.Y + 1, grid.GetLength(1));
+
+            for (int x = minX; x <= maxX; x++) {
+                for (int y = minY; y <= maxY; y++) {
+                    GridNodeCalc2D node = grid[x, y].ToGridNodeCalc2D();
+                    node.GridPosition = new Point2D(x, y);
+
+                    // Skip over this node if it's the one we're currently looking at, or it's in the closed list
+                    if (node.GridPosition == currentGridPos || closedNodes.Contains(node.ReferenceNode)) {
+                        continue;
+                    }
+
+                    // Also skip if it's impassable
+                    if (node.ReferenceNode.Passable == false) {
+                        continue;
+                    }
+
+                    GridNodeCalc2D refNode = openNodes.GetByPosition(node.GridPosition);
+                    bool isDiagonal = (
+                        node.GridPosition.X != currentGridPos.X &&
+                        node.GridPosition.Y != currentGridPos.Y
+                    );
+
+                    // If we're not meant to cut corners, tehn we need to check this now
+                    if (isDiagonal && !cutCorners) {
+                        // If we are further down the grid, then check the square
+                        // above to make sure it's also passable
+                        if (refNode.GridPosition.Y > 0 && refNode.GridPosition.Y > currentGridPos.Y) {
+                            if (!grid[(int) refNode.GridPosition.X, (int) (refNode.GridPosition.Y - 1)].Passable) {
+                                continue;
+                            }
+                        }
+
+                        // If we are further up the grid, then check the square
+                        // below to make sure it's also passable
+                        if (refNode.GridPosition.Y < maxY && refNode.GridPosition.Y < currentGridPos.Y) {
+                            if (!grid[(int) refNode.GridPosition.X, (int) (refNode.GridPosition.Y + 1)].Passable) {
+                                continue;
+                            }
+                        }
+                    }
+
+                    // Is this node on the open list already?
+                    if (refNode == null) {
+                        // If not then set the parent to the current node and calculate the cost
+                        node.Parent = currentNode;
+                        node.MovementCost = currentNode.TotalCost + 10;
+
+                        // Diagonal... add a bit
+                        if (isDiagonal) {
+                            node.MovementCost += 4;
+                        }
+
+                        // Calculate the heuristic cost
+                        node.HeuristicCost = (int) (
+                            Math.Abs(node.GridPosition.X - target.X) +
+                            Math.Abs(node.GridPosition.Y - target.Y)
+                        );
+
+                        openNodes.Add(node);
+                        continue;
+                    }
+
+                    int currentNodeCost = currentNode.MovementCost + currentNode.ReferenceNode.Penalty;
+                    int refNodeCost = refNode.MovementCost + refNode.ReferenceNode.Penalty;
+
+                    // The node was in the open list, so see if it costs less to move there
+                    if (refNodeCost < currentNodeCost) {
+                        // If so, set then set the parent to the current node
+                        refNode.Parent = currentNode.Parent;
+
+                        // Re-calculate the movement cost using the new parent
+                        refNode.MovementCost = refNode.Parent.TotalCost + 10;
+
+                        // Diagonal... add a bit
+                        if (isDiagonal) {
+                            refNode.MovementCost += 4;
+                        }
+
+                        openNodes.Sort(openNodes.IndexOf(refNode));
+                    }
+                }
+            }
         }
 
         private static int getDescentDistance(GridNodeCalc2D node, GridNode2D[,] grid)
